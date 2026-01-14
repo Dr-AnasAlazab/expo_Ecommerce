5a56b545c2238455955d5da0e25ca23cb4ac35a6
Dr-AnasAlazab <anas.alazab30@gmail.com>
2026-01-14 15:14:59 +0300

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,21 @@
+import express from "express"
+import { ENV } from "./config/env"
+
+const app = express()
+
+const __dirname= Path.resolve()
+
+
+app.get("/api/health",(req,res)=>{
+    res.status(200).json({message:"Success"})
+})
+
+//make our app ready for deployment
+if(ENV.NODE_ENV === "production"){
+    app.use(express.static(Path.join(__dirname,"../admin/dist")))
+
+    app.get ("/{*any}", (req,res)=>{
+        res.sendFile(path.join(__dirname,"../admin","dist","index.html"))
+    })}
+
+app.listen(ENV.PORT, ()=> console.log('Server is up and running') )
