e91ba180b04e992337c712f9bca763133accc238
Dr-AnasAlazab <anas.alazab30@gmail.com>
2026-01-24 18:48:22 +0300

    ss

--- backend/src/server.js
+++ backend/src/server.js
@@ -25,7 +25,7 @@ app.get("/api/health", (req, res) => {
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(Path.join(__dirname, "../admin/dist")));
 
-  app.get("/{*any}", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(Path.join(__dirname, "../admin", "dist", "index.html"));
   });
 }
