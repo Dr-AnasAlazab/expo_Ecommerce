467579003417ae05213e3a0ae73e5123bbdc0d81
Dr-AnasAlazab <anas.alazab30@gmail.com>
2026-01-24 18:59:15 +0300

    ss

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,10 +3,10 @@
 import express from "express";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
-import Path from "path";
 import { clerkMiddleware } from "@clerk/express";
 import { functions, inngest } from "./config/inngest.js";
 import { serve } from "inngest/express";
+import path from "path";
 
 const app = express();
 
@@ -16,17 +16,17 @@ app.use(clerkMiddleware());
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
-const __dirname = Path.resolve();
+const __dirname = path.resolve();
 app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "Success" });
 });
 
 //make our app ready for deployment
 if (ENV.NODE_ENV === "production") {
-  app.use(express.static(Path.join(__dirname, "../admin/dist")));
+  app.use(express.static(path.join(__dirname, "../admin/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(Path.join(__dirname, "../admin", "dist", "index.html"));
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
   });
 }
 
