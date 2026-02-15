22101dfd5402dc2401c2e549c4d75b9c5bcf8057
Dr-AnasAlazab <anas.alazab30@gmail.com>
2026-02-15 12:41:09 +0300

    rest of the controllers

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,16 +9,27 @@ import { functions, inngest } from "./config/inngest.js";
 import { serve } from "inngest/express";
 import adminRoutes from "./routes/admin.route.js";
 import userRoute from "./routes/user.route.js";
+import orderRoute from "./routes/order.route.js";
+import reviewRoute from "./routes/review.route.js";
+import cartRoute from "./routes/cart.route.js";
+import productRoute from "./routes/product.route.js";
+import cors from "cors";
 const app = express();
 
 app.use(express.json());
 
 app.use(clerkMiddleware());
 
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.use("/api/admin", adminRoutes);
-app.use("/api/admin", userRoute);
+app.use("/api/user", userRoute);
+app.use("/api/order", orderRoute);
+app.use("/api/reviews", reviewRoute);
+app.use("/api/products", productRoute);
+app.use("/api/cart", cartRoute);
 
 const __dirname = Path.resolve();
 app.get("/api/health", (req, res) => {
