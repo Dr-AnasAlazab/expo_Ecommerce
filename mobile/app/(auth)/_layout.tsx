c0c64cabb5afbfcb82956288e1994d5c825373d5
Dr-AnasAlazab <anas.alazab30@gmail.com>
2026-04-13 08:32:55 +0200

    auth page added

--- mobile/app/(auth)/_layout.tsx
+++ mobile/app/(auth)/_layout.tsx
@@ -1,6 +1,6 @@
 /** @format */
 
-import { useAuth } from "@clerk/expo";
+import { useAuth } from "@clerk/clerk-expo";
 import { Redirect, Stack } from "expo-router";
 
 export default function AuthRoutesLayout() {
